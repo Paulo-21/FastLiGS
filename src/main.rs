03d092cf03f66cd2096df8e396e2dafe0320c232
Paulo-21 <paul.cibier@gmail.com>
2024-11-21 21:40:05 +0000

    use mimalloc

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ mod gr_solver;
 mod nn;
 mod gradualsemantics_opt;
 mod model;
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 fn main() {
     cli::launcher();
